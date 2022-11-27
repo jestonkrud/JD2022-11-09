11b59b8cd859ee0cc94f6f6e408cc8b1a932f25d
Igor Panteleev <souluaryty@gmail.com>
2022-11-27 22:31:24 +0300

    taskA ready

--- src/panteleev/jd01_05/TaskA.java
+++ src/panteleev/jd01_05/TaskA.java
@@ -1,9 +1,28 @@
 package panteleev.jd01_05;
 
+import static java.lang.Math.*;
+
 public class TaskA {
     public static void main(String[] args) {
-        double a = 756.13;
-        double x = 0.3;
+
+        exercise1();
+        exercise2();
+    }
+
+    private static void exercise1() {
+
+        final double a = 756.13;
+        final double x = 0.3;
+
+        double firstNumber = cos(pow(pow(x, 2) + PI / 6, 5));
+        double secondNumber = sqrt(x * pow(a, 3));
+        double thirdNumber = log(abs((a - 1.12 * x) / 4));
+        double z = firstNumber - secondNumber - thirdNumber;
+
+        System.out.println(z);
+    }
+
+    private static void exercise2() {
 
     }
 }
